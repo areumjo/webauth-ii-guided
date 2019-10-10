5596e13659d63ed0b4a7e65d1870acbab26b89e1
Areum Jo <areumjo1@gmail.com>
2019-10-10 10:54:31 -0700

    Added connect-session-knex and edited session Config

--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
+const connectSessionKnex = require('connect-session-knex');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const db = require('../database/dbConfig.js');
 
 const server = express();
+
+// need one more step to execute this library
+const KnexSessionSotre = connectSessionKnex(session);
 
 const sessionConfig = {
   name: 'trackpad life', // sessionid (sid)
@@ -18,7 +23,14 @@ const sessionConfig = {
     httpOnly: true // the browser can't access via JS
   },
   resave: false,
-  saveUninitialized: false // GDPR law !important
+  saveUninitialized: false, // GDPR law !important
+  store: new KnexSessionSotre({
+    knex: db,
+    tablename: 'session',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  }) // you can check this table from sqliteStudio ==> session being stored in DB
 }
 // if you restart the server, the session/cookie are not valid bc session is stroed at server 
 // deleting cookie does not remove session as well, the session will be still in the server
